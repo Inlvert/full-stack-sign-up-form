00f54edc3046470477215395a2b870be321de15a
Borys Ushakov <inlvert@gmail.com>
2024-05-21 13:14:04 +0300

    add user model + validation yup

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const CONSTANTS = require('../constants');
+const User = require('./user');
+
+async function connectToDB() {
+  await mongoose.connect(CONSTANTS.DB_URL)
+}
+
+connectToDB().catch(err => console.log(err));
+
+module.exports = {
+  User
+}
---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const yup = require('yup');
+
+const EMAIL_SCHEMA = yup.string().email().required();
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required']
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: async(emailValue) => EMAIL_SCHEMA.isValid(emailValue),
+      message: 'Email is invalid'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    match: [/^[a-zA-Z0-9]{8,16}$/, 'Password must be 8-16 symbols and must contain letters, and numbers only']
+  }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
