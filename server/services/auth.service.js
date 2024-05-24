31571ef7e133446d65a82048130f142a796f7b53
Borys Ushakov <inlvert@gmail.com>
2024-05-24 14:46:47 +0300

    add services

---
+++ server/services/auth.service.js
@@ -0,0 +1,48 @@
+const createHttpError = require("http-errors");
+const { User, RefreshToken } = require("../models");
+const JwtService = require("./jwt.service");
+
+module.exports.createSession = async (user) => {
+  const tokenPayload = {
+    id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName
+  };
+
+  const tokenPair = await JwtService.createTokenPair(tokenPayload);
+
+  await RefreshToken.create({
+    token: tokenPair.refreshToken,
+    userId: user._id,
+  });
+  return {
+    user,
+    tokenPair,
+  };
+};
+
+module.exports.refreshSession = async (refreshTokenInstance) => {
+  const user = await User.findById(refreshTokenInstance.userId);
+
+  if (!user) {
+    throw createHttpError(404, "User not found");
+  }
+
+  const tokenPayload = {
+    id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  };
+
+  const tokenPair = await JwtService.createTokenPair(tokenPayload);
+
+  await RefreshToken.findOneAndUpdate(
+    { token: refreshTokenInstance.token },
+    { token: tokenPair.refreshToken }
+  );
+
+  return {
+    user,
+    tokenPair,
+  };
+};
---
+++ server/services/jwt.service.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+const {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES_IN,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRES_IN
+} = require('../constants');
+const { access } = require("fs");
+
+const jwtSign = promisify(jwt.sign);
+const jwtVerify = promisify(jwt.verify);
+
+
+const tokenConfig = {
+  access: {
+    secret: ACCESS_TOKEN_SECRET,
+    expiresIn: ACCESS_TOKEN_EXPIRES_IN
+  },
+  refresh: {
+    secret: REFRESH_TOKEN_SECRET,
+    expiresIn: REFRESH_TOKEN_EXPIRES_IN
+  },
+}
+
+const createToken = (payload, { secret, expiresIn }) => jwtSign(payload, secret, { expiresIn });
+const verifyToken = (token, { secret }) => jwtVerify(token, secret);
+
+module.exports.createTokenPair = async (payload) => {
+
+  return {
+    accessToken: await createToken(payload, tokenConfig.access),
+    refreshToken: await createToken(payload, tokenConfig.refresh),
+  }
+}
+
+module.exports.verifyAccessToken = token => verifyToken(token, tokenConfig.access);
+module.exports.verifyRefreshToken = token => verifyToken(token, tokenConfig.refresh);
