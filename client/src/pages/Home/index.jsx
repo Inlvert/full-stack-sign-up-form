6ce6943a024a396516ba1b2443a43e2cf2ec866b
Borys Ushakov <inlvert@gmail.com>
2024-05-27 22:42:46 +0300

    added pages home and login

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+
+function HomePage(props) {
+  return (
+    <div>
+      <Header />
+      hello Guest
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+import LoginForm from '../../components/LoginForm'
+
+function LoginPage(props) {
+  return (
+    <div>
+      <Header />
+      <LoginForm />
+      Login now
+    </div>
+  );
+}
+
+export default LoginPage;
