dc27f1ee9e2a4b3223c73ae3dcc8c120a8646cf4
Boris Ushakov <inlvert@gmail.com>
2024-10-07 12:55:54 +0300

    add

--- server/app.js
+++ server/app.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://full-stack-sign-up-form-server.vercel.app"],
+    methods: ["POST", "GET"],
+    credentals: true
+  }
+));
 app.use(express.json());
 app.use(router)
 
