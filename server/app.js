cea18335df6b8327e282072ef8d04d982b4c8eb1
Boris Ushakov <inlvert@gmail.com>
2024-10-08 22:30:20 +0300

    add

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+// app.use(cors({
+//   origin: 'https://form-client-lilac.vercel.app',  // Replace with your frontend's URL
+//   methods: 'GET,POST,PUT,DELETE', // You can customize the methods allowed
+//   credentials: true  // If you're using cookies or auth tokens that need credentials
+// }));
 app.use(express.json());
 app.use(router)
 
